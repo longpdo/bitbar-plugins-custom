9f71d7fd1bfdb85104f2e3f77112fba7e3e5b55c
Long Do <ldo@adorsys.de>
2020-04-23 19:18:31 +0200

    Refactor yahoo weather

--- plugins/o-yahoo-weather.1h.py
+++ plugins/o-yahoo-weather.1h.py
@@ -24,8 +24,6 @@ from urllib.parse import urlencode, quote
 
 # General Placeholders
 url = 'https://weather-ydn-yql.media.yahoo.com/forecastrss'
-method = 'GET'
-concat = '&'
 
 # Credentials
 app_id = 'f776QQ32'
@@ -33,40 +31,30 @@ consumer_key = 'dj0yJmk9RlJhbUVpUEpsSUxEJmQ9WVdrOVpqYzNObEZSTXpJbWNHbzlNQS0tJnM9
 consumer_secret = '75c592717d22c5cce623d2c2a1d5a5b36786d865'
 
 # Query and authentication related
-query = {'location': f'Nuremberg,BY', 'format': 'json', 'u': 'c'}
-oauth = {
-    'oauth_consumer_key': consumer_key,
-    'oauth_nonce': uuid.uuid4().hex,
-    'oauth_signature_method': 'HMAC-SHA1',
-    'oauth_timestamp': str(int(time.time())),
-    'oauth_version': '1.0'
+query = {
+    'location': 'Nuremberg,BY',
+    'format': 'json',
+    'u': 'c'
 }
 
 
-# Error handling decorator
-def exception_handler(msg="Something is wrong"):
-    def decorator(func):
-        def new_func(*args, **kwargs):
-            try:
-                return func(*args, **kwargs)
-            except:
-                print(f"Error: {msg}")
-                exit(1)
-        return new_func
-    return decorator
-
-
 def get_auth_header():
-    global oauth
-    merged_params = query.copy()
-    merged_params.update(oauth)
-    sorted_params = [k + '=' + quote(merged_params[k], safe='')
-                     for k in sorted(merged_params.keys())]
-    signature_base_str = method + concat + \
-        quote(url, safe='') + concat + quote(concat.join(sorted_params))
-    composite_key = quote(consumer_secret, safe='') + concat
+    oauth = {
+        'oauth_consumer_key': consumer_key,
+        'oauth_nonce': uuid.uuid4().hex,
+        'oauth_signature_method': 'HMAC-SHA1',
+        'oauth_timestamp': str(int(time.time())),
+        'oauth_version': '1.0'
+    }
+
+    merged_dict = {**query, **oauth}
+    sorted_dict = [k + '=' + quote(merged_dict[k], safe='')
+                   for k in sorted(merged_dict.keys())]
+    signature_base = 'GET&' + \
+        quote(url, safe='') + '&' + quote('&'.join(sorted_dict))
+    composite_key = consumer_secret + '&'
     oauth_signature = b64encode(hmac.new(composite_key.encode(
-    ), msg=signature_base_str.encode(), digestmod=hashlib.sha1).digest()).decode()
+    ), msg=signature_base.encode(), digestmod=hashlib.sha1).digest()).decode()
     oauth['oauth_signature'] = oauth_signature
     auth_header = 'OAuth ' + \
         ', '.join(['{}="{}"'.format(k, v) for k, v in oauth.items()])
@@ -74,7 +62,6 @@ def get_auth_header():
     return auth_header
 
 
-@exception_handler(msg="Yahoo Weather API")
 def get_weather(auth_header):
     request_url = url + '?' + urlencode(query)
     request = Request(request_url)
@@ -85,21 +72,19 @@ def get_weather(auth_header):
     return j
 
 
-auth_header = get_auth_header()
-weather_data = get_weather(auth_header)
-condition_text = weather_data['current_observation']['condition']['text']
-condition_temperatur = weather_data['current_observation']['condition']['temperature']
-forecasts = weather_data['forecasts']
+if __name__ == '__main__':
+    auth_header = get_auth_header()
+    weather_data = get_weather(auth_header)
+    current_temperatur = weather_data['current_observation']['condition']['temperature']
+    forecasts = weather_data['forecasts']
 
-print(str(int(condition_temperatur)) + '°C')
-# Dropdown info
-print('---')
-print('Today')
-print(str(condition_text) + ': ' + str(int(condition_temperatur)) + '°C')
-print('---')
-for day in forecasts:
-    print(str(datetime.utcfromtimestamp(
-        int(day['date'])).strftime('%A %d. %B')))
-    print(str(day['text']) + ': ' + str(int(day['low'])) +
-          '-' + str(int(day['high'])) + '°C')
+    print(str(current_temperatur) + '°C')
+    # Dropdown info
     print('---')
+    for day in forecasts:
+        date = datetime.fromtimestamp(int(day['date']))
+
+        print(date.strftime('%A %d. %B'))
+        print(day['text'] + ': ' + str(day['low']) +
+              '-' + str(day['high']) + '°C')
+        print('---')
